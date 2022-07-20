7fef72f586bd42e0801cb61282ed057d84423057
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-07-20 13:14:17 +0900

    [Feat] BaseViewController

--- OverTheRainbow/OverTheRainbow/Screens/Base/BaseViewController.swift
+++ OverTheRainbow/OverTheRainbow/Screens/Base/BaseViewController.swift
@@ -9,6 +9,45 @@ import UIKit
 
 class BaseViewController: UIViewController {
     
+    // MARK: - property
+    
+    private lazy var backButton: UIButton = {
+        let button = BackButton()
+        let buttonAction = UIAction { _ in
+            self.navigationController?
+                .popViewController(animated: true)
+        }
+        button.addAction(buttonAction, for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        render()
+        
+        
+    }
+    
+    func render() {
+        //Override Layout
+    }
+    
+    func configUI() {
+        view.backgroundColor = UIColor(named: "backgroundColor")
+    }
+    
+    func setupNavigationBar() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.shadowColor = .cle
+        
+        navigationBar.standardAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+    }
 }
 
 
