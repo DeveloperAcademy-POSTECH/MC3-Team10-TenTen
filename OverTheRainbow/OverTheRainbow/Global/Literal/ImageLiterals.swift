c2499bf2e3755f43645e8fe558d5bfb5c9babfe8
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-07-20 09:24:57 +0900

    [Feat] image literal setting

--- OverTheRainbow/OverTheRainbow/Global/Literal/ImageLiterals.swift
+++ OverTheRainbow/OverTheRainbow/Global/Literal/ImageLiterals.swift
@@ -34,6 +34,8 @@ extension UIImage {
     }
     
     func resize(to size: CGSize) -> UIImage {
-        let image = UIGraphicsImageRenderer(size: size).image( _ in draw(in: CGRect(origin:  ., size: <#T##CGSize#>)))
+        let image = UIGraphicsImageRenderer(size: size).image{ _ in draw(in: CGRect(origin:  .zero, size: size))
+        }
+        return image
     }
 }
