2d209bc2695017649fb3f9e2615d1c3dee2500d7
LentoAssai <88371913+LentoAssai@users.noreply.github.com>
2022-07-26 21:39:36 +0900

    [Fix] 편지 리스트뷰에서 Realm service 사용할 수 있도록 변경

--- OverTheRainbow/OverTheRainbow/Screens/heavenLetter/HeavenLetterTableViewController.swift
+++ OverTheRainbow/OverTheRainbow/Screens/heavenLetter/HeavenLetterTableViewController.swift
@@ -18,9 +18,7 @@ class HeavenLetterListTableViewController: UITableViewController {
     var toolBar = UIToolbar()
     let years = (2000...2030)
     var yearList = [String]()
-//    let yearList = ["2006년", "2007년", "2008년", "2009년", "2010년", "2011년", "2012년", "2013년", "2014년", "2015년", "2016년", "2017년", "2018년", "2019년", "2020년", "2021년", "2022년"]
     let monthList = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
-
     var selectedYear = ""
     var selectedMonth = ""
     
@@ -32,17 +30,13 @@ class HeavenLetterListTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         for year in years {
             yearList.append("\(year)년")
         }
-//        let currentDate = Date()
-        
-//        var formatter: DateFormatter {
-//            let formatter = DateFormatter()
-//            formatter.dateFormat = "yyyy년 M월"
-//            return formatter
-//        }
+        let yearAndMonth = stringDate.components(separatedBy: " ")
+        selectedYear = yearAndMonth[0]
+        selectedMonth = yearAndMonth[1]
 
         // TODO: 현재 Pet의 ID UserDefaults에서 가져오기
         do {
@@ -50,69 +44,38 @@ class HeavenLetterListTableViewController: UITableViewController {
         } catch {
             print("Error finding sent letters : \(error)")
         }
-        
-//        let yearAndMonth = formatter.string(from: currentDate).components(separatedBy: " ")
-//        selectedYear = yearAndMonth[0]
-//        selectedMonth = yearAndMonth[1]
-        
-//        loadLetters(year: selectedYear, month: selectedMonth)
         setTitleButton(title: selectedMonth)
     }
-    
-    
-    
+
     private func setTitleButton(title: String) {
         let imageAttachment = NSTextAttachment()
         imageAttachment.image = UIImage(systemName: "chevron.down")?.withTintColor(.black)
         let fullString = NSMutableAttributedString(string: "\(title) ")
         fullString.append(NSAttributedString(attachment: imageAttachment))
-        
+
         titleButton.setAttributedTitle(fullString, for: .normal)
         titleButton.frame = CGRect(x: 0, y: 0, width: 50, height: 0)
         titleButton.setTitleColor(.black, for: .normal)
         titleButton.addTarget(self, action: #selector(self.chooseDate), for: .touchUpInside)
-        
+
         navigationBar.title = title
         navigationBar.titleView = titleButton
     }
-    
-//    func loadLetters(year: String, month: String) {
-//        letters = mockLetters.filter {
-//            $0.year == year && $0.month == month
-//        }.sorted { $0.year == $1.year ? $0.month < $1.month : $0.year < $1.year }
-//    }
-    
+
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return letters.count ?? 1
-//        return letters?.count ?? 1
+        return letters.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Letter", for: indexPath)
-
-        //        if let letter = letters?[indexPath.row] {
         if letters.count == 0 {
             cell.textLabel?.text = "No letter."
         } else {
             cell.textLabel?.text = letters[indexPath.row].title
         }
-//        if let letter = letters[indexPath.row] {
-//            cell.textLabel?.text = letter.title
-//        } else {
-//            cell.textLabel?.text = "No letter."
-//        }
-
         return cell
     }
-
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //        let destinationVC = segue.destination as? LetterDetailViewController
-    //
-    //        if let indexPath = tableView.indexPathForSelectedRow {
-    //            destinationVC.selectedLetter = letters?[indexPath.row]
-    //        }
-    //    }
 }
 
 // MARK: - UIPicker
@@ -120,7 +83,7 @@ extension HeavenLetterListTableViewController: UIPickerViewDataSource, UIPickerV
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0 {
             return yearList.count
@@ -128,7 +91,7 @@ extension HeavenLetterListTableViewController: UIPickerViewDataSource, UIPickerV
             return monthList.count
         }
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if component == 0 {
             return yearList[row]
@@ -136,18 +99,15 @@ extension HeavenLetterListTableViewController: UIPickerViewDataSource, UIPickerV
             return monthList[row]
         }
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedYear = yearList[monthPickerView.selectedRow(inComponent: 0)]
         selectedMonth = monthList[monthPickerView.selectedRow(inComponent: 1)]
-        
-        print("\(selectedYear) \(selectedMonth) selected!")
     }
-    
+
     @objc func chooseDate() {
-        
         titleButton.isEnabled = false
-        
+
         monthPickerView = UIPickerView.init()
         monthPickerView.dataSource = self
         monthPickerView.delegate = self
@@ -155,11 +115,11 @@ extension HeavenLetterListTableViewController: UIPickerViewDataSource, UIPickerV
         monthPickerView.backgroundColor = .systemGray6
         monthPickerView.selectRow(yearList.firstIndex(of: selectedYear)!, inComponent: 0, animated: true)
         monthPickerView.selectRow(monthList.firstIndex(of: selectedMonth)!, inComponent: 1, animated: true)
-        
+
         self.view.addSubview(self.monthPickerView)
-        
+
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        
+
         let done = UIBarButtonItem()
         done.title = "확인"
         done.tintColor = .black
@@ -167,31 +127,30 @@ extension HeavenLetterListTableViewController: UIPickerViewDataSource, UIPickerV
         done.target = self
         done.action = #selector(onDoneButtonTapped)
         toolBar = UIToolbar.init()
-        
+
         monthPickerView.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 1080, width: UIScreen.main.bounds.width, height: 250)
-        
+
         UIView.animate(withDuration: 0.5) {
             self.monthPickerView.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 850, width: UIScreen.main.bounds.width, height: 250)
             self.toolBar.frame = CGRect(x: 0.0, y: UIScreen.main.bounds.size.height - 600, width: UIScreen.main.bounds.size.width, height: 45)
         }
-        
+
         toolBar.barTintColor = .systemGray6
         toolBar.isTranslucent = true
         toolBar.setItems([flexSpace, done, flexSpace], animated: true)
         self.view.addSubview(toolBar)
     }
-    
+
     @objc func onDoneButtonTapped() {
-        
+
         titleButton.isEnabled = true
         setTitleButton(title: selectedMonth)
         // TODO: 현재 Pet의 ID UserDefaults에서 가져오기
         do {
-            letters = try service.findSentLetters("62df7c5eedd840ec0d2c01c2", stringDate)
+            letters = try service.findSentLetters("62df7c5eedd840ec0d2c01c2", "\(selectedYear) \(selectedMonth)")
         } catch {
             print("Error finding sent letters : \(error)")
         }
-//        loadLetters(year: selectedYear, month: selectedMonth)
         tableView.reloadData()
         UIView.animate(withDuration: 0.5) {
             self.monthPickerView.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 1200, width: UIScreen.main.bounds.width, height: 250)
